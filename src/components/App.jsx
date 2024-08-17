dfc878e84049466f8d2634214e4c8dfbf7e9f4a4
Tomat <demid.yermak@marakas.digital>
2024-08-17 16:31:35 +0300

    Add version 1.1 (split pages)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,32 @@
+import {lazy, Suspense} from "react";
 import {Routes, Route} from 'react-router-dom';
 import {GlobalStyles} from './GlobalStyle';
 import SharedLayout from "./SharedLayout";
-import { ChakraProvider } from '@chakra-ui/react';
-import Home from "pages/Home";
-import MovieDetail from "pages/MovieDetail";
-import MovieCast from "components/MovieCast";
-import MovieReview from "components/MovieReview";
-import Movies from "pages/Movies";
+import {ChakraProvider} from '@chakra-ui/react';
+
+const Home = lazy(() => import("pages/Home"));
+const MovieDetail = lazy(() => import("pages/MovieDetail"));
+const MovieCast = lazy(() => import("components/MovieCast"));
+const MovieReview = lazy(() => import("components/MovieReview"));
+const Movies = lazy(() => import("pages/Movies"));
 
 export const App = () => {
   return (
     <ChakraProvider>
       <GlobalStyles/>
-      <Routes>
-        <Route path="/" element={<SharedLayout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="movies" element={<Movies/>}/>
-          <Route path='movies/:movieId' element={<MovieDetail/>}>
-            <Route path='cast' element={<MovieCast/>}/>
-            <Route path='reviews' element={<MovieReview/>}/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<SharedLayout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="movies" element={<Movies/>}/>
+            <Route path='movies/:movieId' element={<MovieDetail/>}>
+              <Route path='cast' element={<MovieCast/>}/>
+              <Route path='reviews' element={<MovieReview/>}/>
+            </Route>
+            <Route path="*" element={<div><h1>404</h1></div>}/>
           </Route>
-          <Route path="*" element={<div><h1>404</h1></div>}/>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </ChakraProvider>
   );
 };
